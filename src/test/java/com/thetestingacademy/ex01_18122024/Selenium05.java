3fed6fb90099125b4da8471f5bc9e2973561e796
Nayan Patil <nayanhdesai@gmail.com>
2025-04-10 21:06:23 +0530

    20th Dec

--- src/test/java/com/thetestingacademy/ex01_18122024/Selenium05.java
+++ src/test/java/com/thetestingacademy/ex01_18122024/Selenium05.java
@@ -1,19 +1,19 @@
 package com.thetestingacademy.ex01_18122024;
 
 public class Selenium05 {
-    /*
-     If you are using selenium 3.14
-     System.setProperty("webDriver.edge.driver", "path/to/EdgeDriver.exe");
+
+    // If you are using selenium 3.14
+    // System.setProperty("webDriver.edge.driver", "path/to/EdgeDriver.exe");
             // Firefox
-            System.setProperty("webDriver.gecko.driver", "path/to/geckodriver.exe");
+          //  System.setProperty("webDriver.gecko.driver", "path/to/geckodriver.exe");
             // Chrome
-            System.setProperty("webDriver.chrome.driver", "path/to/chromedriver.exe");
+         //   System.setProperty("webDriver.chrome.driver", "path/to/chromedriver.exe");
             // IE
-            System.setProperty("webDriver.ie.driver", "path/to/IEDriverServer.exe");
+         //   System.setProperty("webDriver.ie.driver", "path/to/IEDriverServer.exe");
             // Safari
-            System.setProperty("webDriver.safari.driver", "path/to/safariDriver");
-     Selenium 4.x - Selenium Manager - will automatically download the Drivers all
-     when needed when you start the Selenium server.
-    */
+         //   System.setProperty("webDriver.safari.driver", "path/to/safariDriver");
+  // Selenium 4.x - Selenium Manager - will automatically download the Drivers all
+  //   when needed when you start the Selenium server.
+
 
 }
