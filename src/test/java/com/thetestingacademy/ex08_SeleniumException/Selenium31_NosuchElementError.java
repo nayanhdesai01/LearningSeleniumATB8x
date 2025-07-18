9c0a226df4e41f5dbc472c8b56b21a01b31d7070
Nayan Patil <nayanhdesai@gmail.com>
2025-07-18 07:06:12 +0530

    exceptions handling

---
+++ src/test/java/com/thetestingacademy/ex08_SeleniumException/Selenium31_NosuchElementError.java
@@ -0,0 +1,50 @@
+package com.thetestingacademy.ex08_SeleniumException;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class Selenium31_NosuchElementError {
+    EdgeDriver driver;
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        edgeOptions.addArguments("--guest");
+        driver = new EdgeDriver(edgeOptions);
+    }
+
+    @Description("Verify if Web driver is able to Locate the element")
+    @Test
+    public void test_exceptions(){
+        String URL = "https://app.vwo.com/";
+        driver.get(URL);
+        driver.manage().window().maximize();
+        try{
+            driver.findElement(By.xpath("kitten"));
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
